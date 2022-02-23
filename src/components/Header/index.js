ba18e8efb46b24e19239900c80ffb0fac07b4027
Ronaldo <ronaldofigueiredosp@gmail.com>
2022-02-22 22:06:11 -0300

    adjusting package.json and adding useState on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './header.css';
 
 export function Header({ text }) {
+
+  const [ title, setTitle ] = useState('Title')
+
   return (
     <nav className="header">
       <h1>
+        {`${title}`}
         {text}
       </h1>
     </nav>);
